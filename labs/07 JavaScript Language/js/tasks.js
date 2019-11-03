6bb01206197d7d01aae67f26ad34bd84872233c0
Essi Norri <essi.norri@outlook.com>
2019-11-03 20:38:39 +0200

    javascript

--- labs/07 JavaScript Language/js/tasks.js
+++ labs/07 JavaScript Language/js/tasks.js
@@ -2,7 +2,8 @@
 // This script manages a to-do list.
 
 // Need a global variable:
-var tasks = []; 
+var tasks = [];
+var temptasks = [];
 
 // Function called when the form is submitted.
 // Function adds a task to the global array.
@@ -38,9 +39,34 @@ function addTask() {
     
 } // End of addTask() function.
 
+function removeDuplicates(){
+    document.body.style.backgroundColor = "red";
+
+    for (var i=0; i < tasks.length; i++) {
+        document.body.style.backgroundColor = "blue";
+        if(temptasks.includes(tasks[i]) == false){
+                console.log("hei");
+                console.log(tasks[i]);
+                temptasks.push(tasks[i]);
+                console.log(temptasks);
+        }
+    }
+    tasks = temptasks;
+    temptasks = [];
+    output.innerHTML = '';
+     message = '<h2>To-Do</h2><ol>';
+     for (var i = 0, count = tasks.length; i < count; i++) {
+         message += '<li>' + tasks[i] + '</li>';
+     }
+     message += '</ol>';
+     output.innerHTML = message;
+
+}
+
 // Initial setup:
 function init() {
     'use strict';
     document.getElementById('theForm').onsubmit = addTask;
+    document.getElementById('remove').onclick = removeDuplicates;
 } // End of init() function.
 window.onload = init;

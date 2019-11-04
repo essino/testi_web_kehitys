6080f564152a87307f0917e911f656971c8b64c6
Essi Norri <essi.norri@outlook.com>
2019-11-04 22:41:29 +0200

    book titles done one page

--- labs/09 Single-Page Applications/js/building_lists.js
+++ labs/09 Single-Page Applications/js/building_lists.js
@@ -5,39 +5,62 @@ request.send(null);
 var data = JSON.parse(request.responseText);
 console.log(data);
 
+
+
 var books = data.books;
 
-var list = document.createElement('ul');
+/*var list = document.createElement('ul');
 for (var i=0; i < books.length; i++) {
 	console.log(books[i].title);
 	var item = document.createElement('li');
 	item.innerHTML = books[i].title + " " + books[i].year;
 	list.appendChild(item);
-}
+}*/
 
-//var list = document.createElement('table');
-/*var table = document.createElement('table');
-var row1 = table.insertRow(0);
-var cell1 = row1.insertCell(0);
-var cell2 = row1.insertCell(1);
-cell1.innerHTML = "Name";
-cell2.innerHTML = "Year";*/
+let divi = document.createElement('div');
 
-//for (var i=0; i < books.length; i++) {
-//console.log(books[i].title);
-	var divi = document.createElement('div');
-	divi.innerHTML = "Morjens!"
 document.body.appendChild(divi);
-	/*
-	var row = document.createElement('tr');
-	var row = table.insertRow(0);
-	var cell1 = row.insertCell(0);
-	var cell2 = row.insertCell(1);
-	cell1.innerHTML = books[i].title;
-	cell2.innerHTML = books[i].year;
-	list.appendChild(item);
-	list.appendChild(item2);*/
-//}
 
 
-//document.body.appendChild(list);
+//create table
+var table = document.createElement('table');
+//table.innerHTML = "taulukko";
+
+//create headings
+let tr = document.createElement('tr');
+//tr.innerHTML = "rivi";
+table.appendChild(tr);
+var th = document.createElement('th');
+th.innerHTML = "Kirjan nimi";
+tr.appendChild(th);
+var th = document.createElement('th');
+th.innerHTML = "Julkaisuvuosi";
+tr.appendChild(th);
+
+for (var i=0; i < books.length; i++) {
+	console.log(books[i].title);
+	let tr = document.createElement('tr');
+	//tr.innerHTML = "rivi" + [i];
+	tr.title = books[i].title;
+
+	tr.addEventListener('click', function(){
+		console.log("testitestimissä on");
+		console.log("Kirjan nimi " + tr.title);
+		let divis = document.getElementById('divi');
+		divis.textContent = tr.title;
+
+	});
+	table.appendChild(tr);
+	var td = document.createElement('td');
+	td.title = books[i].title;
+	td.innerHTML = books[i].title;
+
+	tr.appendChild(td);
+	var td = document.createElement('td');
+	td.innerHTML = books[i].year;
+
+	tr.appendChild(td);
+
+}
+document.body.appendChild(divi);
+document.body.appendChild(table);
